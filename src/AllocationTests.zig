8faf270ef7c5cffdafd7f0092905a12c931637a2
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-05-04 17:29:13 +0300

    Heap and Stack create

---
+++ src/AllocationTests.zig
@@ -0,0 +1,93 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+pub fn main() !void {}
+
+test "Create array using heap" {
+    const allocator = std.heap.page_allocator;
+
+    const memory = try allocator.alloc(u8, 100);
+    defer allocator.free(memory);
+
+    memory[1] = 2;
+    const memory1 = memory[0..88];
+
+    // Heap-allocated dynamic array using ArrayList
+    var numbers = std.ArrayList(u32).init(allocator);
+    defer numbers.deinit();
+
+    try numbers.append(10);
+    try numbers.append(20);
+    try numbers.append(30);
+
+    try expect(memory1.len == 88);
+    try expect(memory.len == 100);
+    try expect(memory[1] == 2);
+    try expect(@TypeOf(memory) == []u8);
+    try expect(numbers.items.len == 3);
+}
+
+test "Heap allocator create/destroy (Single item)" {
+    const byte = try std.heap.page_allocator.create(u8);
+    defer std.heap.page_allocator.destroy(byte);
+    byte.* = 128;
+}
+
+test "Create array using stack" {
+    // In Zig, you can allocate memory on the stack by declaring variables with a fixed size.
+    // These variables are known as stack-allocated variables, and their lifetime is bound to the scope in which they are defined.
+    const Point = struct {
+        x: f32,
+        y: f32,
+    };
+
+    var point: Point = .{
+        .x = 1.5,
+        .y = 2.7,
+    };
+
+    point.x += 3.0;
+    point.y -= 1.2;
+
+    var numbers: [5]u16 = .{ 10, 20, 30, 40, 50 };
+    numbers[2] = 999;
+    // 'numbers' and 'point' is automatically deallocated when it goes out of scope
+    try expect((numbers.len == 5));
+    try expect((point.x == 4.5));
+    try expect((point.y == 1.5));
+}
+
+test "fixed buffer allocator" { // Exceeds fixed number => error
+    var buffer: [1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+
+    const memory = try allocator.alloc(u8, 1000);
+    defer allocator.free(memory);
+
+    try expect(memory.len == 1000);
+    try expect(@TypeOf(memory) == []u8);
+}
+
+test "arena allocator" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    _ = try allocator.alloc(u8, 1);
+    _ = try allocator.alloc(u8, 10);
+    _ = try allocator.alloc(u8, 100);
+}
+
+test "GPA" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        const deinit_status = gpa.deinit();
+        //fail test; can't try in defer as defer is executed after we return
+        if (deinit_status == .leak) expect(false) catch @panic("TEST FAIL");
+    }
+
+    const bytes = try allocator.alloc(u8, 100);
+    defer allocator.free(bytes);
+}
