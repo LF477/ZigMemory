deab70f7e8793c516a522791ed358cddc6ba06a0
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-05-12 02:16:05 +0300

    Try in push and pull image 28

--- build.zig
+++ build.zig
@@ -23,7 +23,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    exe.linkLibC();
+    // exe.linkLibC();
     b.installArtifact(exe); // executable to be installed into the standard location
 
     //##############################################################################
